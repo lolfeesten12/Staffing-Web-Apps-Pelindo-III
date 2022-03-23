0961601e3817046c49dd0f5a87f0236555d2e56e
artanila <78575127+artanila@users.noreply.github.com>
2022-03-23 23:13:51 +0800

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'hrd' => \App\Http\Middleware\IsHRD::class,
+        'kepala_unit' => \App\Http\Middleware\IsKepalaUnit::class,
+
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -32,81 +32,77 @@ use App\Models\Riwayat\RiwayatPendidikan;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\User\ProfileController::class, 'index'])->name('home');
 
 
-// ---------------------user------------------
-Route::prefix('User')
-    // ->middleware(['Admin_Role','verified'])
-    ->group(function () {
-        Route::resource('profile', ProfileController::class);
-        Route::resource('riwayat-keluarga', RiwayatKeluargaController::class);
-        Route::resource('riwayat-pendidikan', RiwayatKeluargaController::class);
-        Route::resource('riwayat-prestasi', RiwayatKeluargaController::class);
-        Route::resource('riwayat-cuti', RiwayatKeluargaController::class);
-        Route::resource('riwayat-pelanggaran', RiwayatKeluargaController::class);
-        Route::resource('riwayat-sanksi', RiwayatKeluargaController::class);
 
 
-    });
+
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/', [App\Http\Controllers\User\ProfileController::class, 'index'])->name('home');
+    Route::get('/home', [App\Http\Controllers\User\ProfileController::class, 'index'])->name('home');
 
 
-// MASTER DATA
-Route::prefix('Masterdata')
-    // ->middleware(['Admin_Role','verified'])
-    ->group(function () {
-        Route::resource('unit-kerja', MasterUnitKerjaController::class);
-        Route::resource('jabatan', MasterJabatanController::class);
-        Route::resource('pegawai', MasterPegawaiController::class);
-        Route::resource('shift', MasterShiftController::class);
-        Route::resource('sanksi', MasterSanksiController::class);
-        Route::resource('pelanggaran', MasterPelanggaranController::class);
-    });
+    // ---------------------user------------------
+    Route::prefix('User')
+        // ->middleware(['Admin_Role','verified'])
+        ->group(function () {
+            Route::resource('profile', ProfileController::class);
+        });
+
+
+    // MASTER DATA
+    Route::prefix('Masterdata')
+        ->middleware(['hrd'])
+        ->group(function () {
+            Route::resource('unit-kerja', MasterUnitKerjaController::class);
+            Route::resource('jabatan', MasterJabatanController::class);
+            Route::resource('pegawai', MasterPegawaiController::class);
+            Route::resource('shift', MasterShiftController::class);
+            Route::resource('sanksi', MasterSanksiController::class);
+            Route::resource('pelanggaran', MasterPelanggaranController::class);
+        });
+
+    Route::prefix('Jadwal')
+        ->group(function () {
+            Route::resource('jadwal-pegawai', JadwalPegawaiController::class);
+            Route::post('jadwal-pegawai/{id_pegawai}/tanggal', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'getJadwal']);
+            Route::get('jadwal-pegawai/{id_pegawai}/tanggal', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'JadwalPegawai']);
+            Route::post('jadwal-pegawai/{id_pegawai}/masuk', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'JadwalMasuk']);
+            Route::post('jadwal-pegawai/{id_pegawai}/libur', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'JadwalLibur']);
+        });
+
+    Route::prefix('Riwayat')
+        // ->middleware(['Admin_Role','verified'])
+        ->group(function () {
+                Route::resource('keluarga', RiwayatKeluargaController::class);
+                Route::resource('pendidikan', RiwayatPendidikanController::class);
+                Route::resource('riwayatpelanggaran', RiwayatPelanggaranController::class);
+                Route::resource('cuti', RiwayatCutiController::class);
+                Route::resource('prestasi', RiwayatPrestasiController::class);
+                Route::resource('riwayatsanksi', RiwayatSanskiController::class);
+        });
+
+});
 
 // REQUIREMENT
 Route::prefix('Requirement')
-    ->group(function () {
-        Route::resource('pengumuman', PengumumanController::class);
-    });
+->group(function () {
+    Route::resource('pengumuman', PengumumanController::class);
+});
 
 Route::prefix('Requirement')
-    ->group(function () {
-        Route::get('/hasil_seleksi', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'HasilSeleksi'])->name('calon-pegawai-hasil');
-        Route::get('/download_cv/{file_cv}', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'getFile'])->name('calon-pegawai-cv');
-        Route::get('/download_pendukung/{file_pendukung}', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'getFilePendukung'])->name('calon-pegawai-pendukung');
-        Route::post('Calon/{id_calon_pegawai}/Status', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'setStatus'])->name('calon-pegawai-status');
-        Route::resource('calon-pegawai', CalonPegawaiController::class);
-    });
+->group(function () {
+    Route::get('/hasil_seleksi', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'HasilSeleksi'])->name('calon-pegawai-hasil');
+    Route::get('/download_cv/{file_cv}', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'getFile'])->name('calon-pegawai-cv');
+    Route::get('/download_pendukung/{file_pendukung}', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'getFilePendukung'])->name('calon-pegawai-pendukung');
+    Route::post('Calon/{id_calon_pegawai}/Status', [App\Http\Controllers\WebRequirement\CalonPegawaiController::class, 'setStatus'])->name('calon-pegawai-status');
+    Route::resource('calon-pegawai', CalonPegawaiController::class);
+});
 
 Route::prefix('WebRequirement')
-    ->group(function () {
-        Route::resource('web-requirement', WebRequirementController::class);
-    });
+->group(function () {
+    Route::resource('web-requirement', WebRequirementController::class);
+});
 
 
-Route::prefix('Jadwal')
-    ->group(function () {
-        Route::resource('jadwal-pegawai', JadwalPegawaiController::class);
-        Route::post('jadwal-pegawai/{id_pegawai}/tanggal', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'getJadwal']);
-        Route::get('jadwal-pegawai/{id_pegawai}/tanggal', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'JadwalPegawai']);
-        Route::post('jadwal-pegawai/{id_pegawai}/masuk', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'JadwalMasuk']);
-        Route::post('jadwal-pegawai/{id_pegawai}/libur', [App\Http\Controllers\Jadwal\JadwalpegawaiController::class, 'JadwalLibur']);
-    });
-    Route::prefix('Riwayat')
-    // ->middleware(['Admin_Role','verified'])
-    ->group(function () {
-            Route::resource('keluarga', RiwayatKeluargaController::class);
-            Route::resource('pendidikan', RiwayatPendidikanController::class);
-            Route::resource('riwayatpelanggaran', RiwayatPelanggaranController::class);
-            Route::resource('cuti', RiwayatCutiController::class);
-            Route::resource('prestasi', RiwayatPrestasiController::class);
-            Route::resource('riwayatsanksi', RiwayatSanskiController::class);
-
-
-    });
